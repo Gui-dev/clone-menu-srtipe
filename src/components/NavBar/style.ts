1c40e093283b26df6b82ed736acc8a20a6f87708
Gui-dev <adrianon10@gmail.com>
2020-08-03 19:19:17 -0300

    feat: add NavBar to project

---
+++ src/components/NavBar/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: linear-gradient(150deg, #53F 15%, #05D5FF);
+
+  > ul {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 2.1rem;
+  }
+`
